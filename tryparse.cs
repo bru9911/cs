aa9ad4e054bdef2350ab3a1e04a6336180dd6ee6
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2022-07-21 17:17:31 -0300

    Create tryparse.cs

---
+++ tryparse.cs
@@ -0,0 +1,19 @@
+static void Main()
+
+{
+string name = "";
+string[] names = { "Bruno", "Padovani" };
+
+int i = 10;
+if (int.TryParse("ab", out x))
+{
+Console.WriteLine("Sucesso!!!");
+} 
+else
+{
+Console.WriteLine("Erro!!");
+}
+
+Console.WriteLine(x);
+    }
+}
