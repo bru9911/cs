37a83e498933fb080e4c5b3047902b0c8dc2bfe9
Bruno Padovani Canna <60715983+bru9911@users.noreply.github.com>
2022-07-21 17:00:16 -0300

    Create hello.cs

---
+++ hello.cs
@@ -0,0 +1,17 @@
+Console.WriteLine("Hello, World!");
+
+bool b,b2;
+char c;
+byte b3;
+int i;
+long l;
+float f;
+double d;
+decimal d2;
+string s;
+
+
+int i 
+int i2 = i = 10;
+Console.WriteLine(i2);
+
